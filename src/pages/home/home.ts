ff7eddcd8b98c4723a5d67b5da7699bc415c347e
Pranav <hulkcodexl16@gmail.com>
2018-08-13 02:29:50 -0700

    Added In App Browser Plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-home',
